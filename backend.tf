9b5aa2e6adbd202a8490125404d11760649426de
GitHubMAB <maboukil@gmail.com>
2025-11-27 14:58:24 +0100

    Change storage account name in backend.tf
    Updated the storage account name for the Terraform backend.

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "myterraformbackend"  # Nom de ton Storage Account
+    storage_account_name = "myterraformbackenddeep"  # Nom de ton Storage Account
     container_name       = "tfstatestorage"     # Nom de ton Container Blob
     key                   = "workflow.tfstate" # Nom du fichier d'état
   }
