b2d7f35054a4f27edead9e23226d8f9769355ea6
GitHubMAB <maboukil@gmail.com>
2025-11-27 21:07:19 +0100

    Change backend from local to Azure RM

--- backend.tf
+++ backend.tf
@@ -1,7 +1,16 @@
 # backend.tf
 
+#terraform {
+#  backend "local" {
+   # path = "./terraform.tfstate"  # Le fichier d'état local (qui sera commité dans GitHub)
+  #}
+#"
+#}
 terraform {
-  backend "local" {
-    path = "./terraform.tfstate"  # Le fichier d'état local (qui sera commité dans GitHub)
+  backend "azurerm" {
+    resource_group_name   = "Backend_TFSTATE"
+    storage_account_name  = "backendtfstatedeep"
+    container_name        = "tfstates"
+    key                    = "terraform.tfstate"
   }
 }
