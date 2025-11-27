808d88e00400df37ad64dbf787e345ed9402f92f
GitHubMAB <maboukil@gmail.com>
2025-11-27 14:37:58 +0100

    Remove client_id, client_secret, and tenant_id
    Removed client authentication details from provider configuration.

--- provider.tf
+++ provider.tf
@@ -3,7 +3,4 @@ provider "azurerm" {
 
   # Tu peux aussi spécifier les informations d'authentification via les variables d'environnement si nécessaire.
   subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
