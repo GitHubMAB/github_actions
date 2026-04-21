312c3e70aef62b3242715f5b9a52bb3238b12b55
Mohamed Amine Boukil <maboukil@gmail.com>
2026-04-21 15:45:06 +0200

    add

--- aks.tf
+++ aks.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # -------------------------
-# AKS Cluster
+# AKS Cluster AAA
 # -------------------------
 
 resource "azurerm_kubernetes_cluster" "aks" {
